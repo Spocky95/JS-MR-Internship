d4d7f3c1c203c63e357e258afd39771dfb992ca6
Spocky95 <retkowskimarcin95@gmail.com>
2021-07-26 13:17:54 +0200

    pierwszy Commit programu test

---
+++ 3/szubienica.js
@@ -0,0 +1,9 @@
+var haslo = "Bez pracy nie ma kołaczy";
+function wypisz_haslo()
+{
+    document.getElementById("plansza").innerHTML = haslo;
+}
+window.onload = wypisz_haslo  /*wywolanie zdarzenia przez zaladowanie programu |onload > wypisz_haslo|*/
+
+
+
