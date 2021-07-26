d386af7ea3a58d8de45ca4b039aca215a5f8817a
Spocky95 <retkowskimarcin95@gmail.com>
2021-07-26 15:55:48 +0200

    aktualizacja brancha

--- 3/szubienica.js
+++ 3/szubienica.js
@@ -1,9 +1,34 @@
+
+
+
+
 var haslo = "Bez pracy nie ma kołaczy";
+
+var dlugosc = haslo.length;
+var haslo1 = "";
+    /*od i rownego zero; powtarzaj dopuki i jestmniejsze niz dlugosc; inkrementuj*/
+for(i=0;i<dlugosc;i++)
+{
+    if(haslo[i]==" ")
+    {
+        haslo1 = haslo1 + " ";
+    }
+    else haslo1 = haslo1 + "_";
+}
+
+var topka = "Haslo: ";/*test*/
+haslo1 = topka + haslo1;/*test*/
+haslo1 = haslo1.toUpperCase();/*zamiana na duze litery*/
+
+
 function wypisz_haslo()
 {
-    document.getElementById("plansza").innerHTML = haslo;
+    document.getElementById("plansza").innerHTML = haslo1;
+    
 }
 window.onload = wypisz_haslo  /*wywolanie zdarzenia przez zaladowanie programu |onload > wypisz_haslo|*/
+
+/*alert("Witaj w grze wisielec, wygraj odgadujac haslo!")*/
 
 
 
