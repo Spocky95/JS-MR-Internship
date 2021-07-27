33b96d7085e4da63a6aa603ad5f33e3de6226728
Spocky95 <retkowskimarcin95@gmail.com>
2021-07-27 10:00:33 +0200

    aktualizacja z dodanymi kolejnymi divami

--- 3/szubienica.js
+++ 3/szubienica.js
@@ -6,18 +6,21 @@ var haslo = "Bez pracy nie ma kołaczy";
 
 var dlugosc = haslo.length;
 var haslo1 = "";
+
+
+
     /*od i rownego zero; powtarzaj dopuki i jestmniejsze niz dlugosc; inkrementuj*/
 for(i=0;i<dlugosc;i++)
 {
-    if(haslo[i]==" ")
+    if(haslo.charAt(i)==" ")
     {
         haslo1 = haslo1 + " ";
     }
     else haslo1 = haslo1 + "_";
 }
 
-var topka = "Haslo: ";/*test*/
-haslo1 = topka + haslo1;/*test*/
+var topka = "Haslo: ";
+haslo1 = topka + haslo1;
 haslo1 = haslo1.toUpperCase();/*zamiana na duze litery*/
 
 
@@ -26,7 +29,28 @@ function wypisz_haslo()
     document.getElementById("plansza").innerHTML = haslo1;
     
 }
-window.onload = wypisz_haslo  /*wywolanie zdarzenia przez zaladowanie programu |onload > wypisz_haslo|*/
+
+function start()
+{   
+    var tresc_diva = ""; /* zmienna lokalna*/
+
+    for(i=0;i<34;i++)
+    {
+        tresc_diva = tresc_diva + '<div class="litera">A</div>';
+        if((i+1) % 7 == 0)
+        {
+            tresc_diva = tresc_diva + '<div style="clear: both;"></div>';
+        }
+        
+
+    }
+    
+    document.getElementById("alfabet").innerHTML = tresc_diva;
+    wypisz_haslo();
+}
+
+
+window.onload = start;  /*wywolanie zdarzenia przez zaladowanie programu |onload > wypisz_haslo|*/
 
 /*alert("Witaj w grze wisielec, wygraj odgadujac haslo!")*/
 
